2b16b670f6d9b60e4516b1a7d141719b0e9877cf
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-03-12 02:46:45 -0500

    Update script.js
    Added code to set up function for a password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,5 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -10,8 +9,50 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var charset = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  number: "0123456789",
+  symbol: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+};
+
+function generatePassword() {
+  passwordCharSet = "";
+
+  var length = window.prompt(
+    "Enter a number from 8 to 128 for password length."
+  );
+
+  var lowercase = window.confirm("Would you like to use lowercase letters?");
+  if (lowercase == true) {
+    passwordCharSet += lowercase;
+  }
+
+  var uppercase = window.confirm("Would you like to use uppercase letters?");
+  if (uppercase == true) {
+    passwordCharSet += uppercase;
+  }
+
+  var symbols = window.confirm("Would you like to use symbols?");
+  if (symbols == true) {
+    passwordCharSet += symbols;
+  }
+
+  var numbers = window.confirm("Would you like to use numbers?");
+  if (numbers == true) {
+    passwordCharset += numbers;
+  }
+
+  for (let i = 0; i < length; i++) {
+    password +=
+      passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)];
+  }
+  return password;
+}
+
+console.log(generatePassword());
